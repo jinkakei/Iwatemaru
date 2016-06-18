6bbccc83b8decd9d956ff0516abec868f715a92f
Kei Nishina <k247loveke@gmail.com>
2016-06-18 15:20:02 +0900

    add Horizontal Extrapolation

--- csv_make.rb
+++ csv_make.rb
@@ -3,50 +3,52 @@ require "~/lib_k247/K247_basic"
 require "~/Iwatemaru/lib_iwatemaru"
 
 # cut
-in_fn = "clim_lin.nc"
+cname = "clim"
+#cname = "201408"
+in_fn = "#{cname}_lin.nc"
   #lon_min = 142.2; lon_max = 142.5
   lon_min = 142.1; lon_max = 142.5025
-  gp_cptemp0   = GPhys::IO.open( in_fn, "cptemp_lin")
-    gp_cptemp = gp_cptemp0.cut( "llon" => lon_min..lon_max )
-  gp_csal0   = GPhys::IO.open( in_fn, "csal_lin")
-    gp_csal = gp_csal0.cut( "llon" => lon_min..lon_max )
-  gp_crhoo0  = GPhys::IO.open( in_fn, "crhoo_lin")
-    gp_crhoo = gp_crhoo0.cut( "llon" => lon_min..lon_max )
-  xn, yn = gp_cptemp.shape
+  gp_ptemp0   = GPhys::IO.open( in_fn, "ptemp_lin")
+    gp_ptemp = gp_ptemp0.cut( "llon" => lon_min..lon_max )
+  gp_sal0   = GPhys::IO.open( in_fn, "sal_lin")
+    gp_sal = gp_sal0.cut( "llon" => lon_min..lon_max )
+  gp_rhoo0  = GPhys::IO.open( in_fn, "rhoo_lin")
+    gp_rhoo = gp_rhoo0.cut( "llon" => lon_min..lon_max )
+  xn, yn = gp_ptemp.shape
   #DCL.gropn( 1 ) # 1: display, 2: pdf
   ##dcl.udpset( 'lmsg', false ) # erase "contour interval ..."
   #  tlevs = 1.0 * NArray.sfloat( 18 ).indgen + 2.0
-  #  GGraph.tone( gp_cptemp, true, "levels"=> tlevs )
+  #  GGraph.tone( gp_ptemp, true, "levels"=> tlevs )
   #  slevs = 0.1 * NArray.sfloat( 11 ).indgen + 33.0
-  #  GGraph.tone( gp_csal, true, "levels"=> slevs )
+  #  GGraph.tone( gp_sal, true, "levels"=> slevs )
   #  rlevs = 0.2 * NArray.sfloat( 16 ).indgen + 23.8 # 23.8 ~ 26.7
-  #  GGraph.tone( gp_crhoo, true, "levels"=> rlevs )
+  #  GGraph.tone( gp_rhoo, true, "levels"=> rlevs )
   #DCL.grcls
 # 2016-06-15: caution! tentative, rhoo -> temp ( for current nhmodel.F)
-  #temp = 10.0 + ( gp_crhoo.val - 25.25 ) * 0.001 / ( - 2.0 * 0.0001)
+  #temp = 10.0 + ( gp_rhoo.val - 25.25 ) * 0.001 / ( - 2.0 * 0.0001)
   #p temp
   #p temp.max
   #p temp.min
 # 2016-06-16
-  cptemp = gp_cptemp.val
-  csal   = gp_csal.val
-  out_fn = "clim_lin.csv"
+  ptemp = gp_ptemp.val
+  sal   = gp_sal.val
+  out_fn = "#{cname}_lin.csv"
     puts "out_fn: #{out_fn}"
     puts "  xn = #{xn}, yn = #{yn}"
   fu = File.open( out_fn, "w" )
     for i in 0..xn-1
-      fu.write cptemp[i, 0].round(2)
+      fu.write ptemp[i, 0].round(2)
     for j in 1..yn-1
       fu.write ", "
-      fu.write cptemp[i, j].round(2)
+      fu.write ptemp[i, j].round(2)
     end
       fu.write "\n"
     end
     for i in 0..xn-1
-      fu.write csal[i, 0].round(2)
+      fu.write sal[i, 0].round(2)
     for j in 1..yn-1
       fu.write ", "
-      fu.write csal[i, j].round(2)
+      fu.write sal[i, j].round(2)
     end
       fu.write "\n"
     end
--- interpolation.rb
+++ interpolation.rb
@@ -3,61 +3,91 @@
 require "~/lib_k247/K247_basic"
 require "~/Iwatemaru/lib_iwatemaru"
 
-in_fn = "clim.nc"
-  gp_cptemp = GPhys::IO.open( in_fn, "cptemp")
-  gp_csal   = GPhys::IO.open( in_fn, "csal")
-  gp_crhoo  = GPhys::IO.open( in_fn, "crhoo")
+
+cname = "clim"
+#cname = "201408"
+in_fn = "ncdf/#{cname}.nc"
+if cname == "clim"
+  gp_ptemp = GPhys::IO.open( in_fn, "cptemp")
+  gp_sal   = GPhys::IO.open( in_fn, "csal")
+  gp_rhoo  = GPhys::IO.open( in_fn, "crhoo")
+else  
+  gp_ptemp = GPhys::IO.open( in_fn, "ptemp")
+  gp_sal   = GPhys::IO.open( in_fn, "sal")
+  gp_rhoo  = GPhys::IO.open( in_fn, "rhoo")
+end
   miss_val = -999.99
 
-  xn0, yn0 = gp_cptemp.shape
-  lon = gp_cptemp.coord( 0 ).val
-  #dep = gp_cptemp.coord( 1 ).val
+  xn0, yn0 = gp_ptemp.shape
+  lon = gp_ptemp.coord( 0 ).val
+  #dep = gp_ptemp.coord( 1 ).val
 # 2016-06-16: test extrapolation
   xn = xn0
   yn = yn0 + 1
   exdep_max = -500.0
-  dep0 = gp_cptemp.coord( 1 ).val
+  dep0 = gp_ptemp.coord( 1 ).val
     dep = NArray.sfloat( yn0 + 1 )
     dep[0..-2] = dep0[0..-1]
     dep[-1] = exdep_max
   
-  cptemp0 = gp_cptemp.val
-  na_cptemp = NArray.sfloat(xn, yn).fill( miss_val )
-  csal0 = gp_csal.val
-  na_csal = NArray.sfloat(xn, yn).fill( miss_val )
-  crhoo0 = gp_crhoo.val
-  na_crhoo = NArray.sfloat(xn, yn).fill( miss_val )
-  #  na_cptemp[0..-1, 0..-2] = cptemp0[0..-1, 0..-1]
+  ptemp0 = gp_ptemp.val
+  na_ptemp = NArray.sfloat(xn, yn).fill( miss_val )
+  sal0 = gp_sal.val
+  na_sal = NArray.sfloat(xn, yn).fill( miss_val )
+  rhoo0 = gp_rhoo.val
+  na_rhoo = NArray.sfloat(xn, yn).fill( miss_val )
+  #  na_ptemp[0..-1, 0..-2] = ptemp0[0..-1, 0..-1]
   #  ! "cannot convert NArrayMiss to Float"
-  # move
+  # move data
   for i in 0..xn-1
   for j in 0..yn-2
-    if cptemp0[i,j] > (miss_val+1)
-      na_cptemp[i,j] = cptemp0[i,j] 
-      na_csal[i,j] = csal0[i,j] 
-      na_crhoo[i,j] = crhoo0[i,j]
+    if ptemp0[i,j] > (miss_val+1)
+      na_ptemp[i,j] = ptemp0[i,j] 
+      na_sal[i,j] = sal0[i,j] 
+      na_rhoo[i,j] = rhoo0[i,j]
     end
   end
   end
+  # horizontal extrapolation
+  # ! now, for Ozaki line only!
+  #  puts "!CAUTION! tentative horizontal extrapolation@2016-06-18"
+    i0 = 1; j0 = 8
+    # horizontal extrapolation
+    na_ptemp[i0,j0] = na_ptemp[i0+1,j0] \
+                    + (na_ptemp[i0+2] - na_ptemp[i0+1]) \
+                    *  ( lon[i0+1] - lon[i0] ) / (lon[i0+2] - lon[i0+1])
+    na_sal[i0,j0] = na_sal[i0+1,j0] \
+                    + (na_sal[i0+2] - na_sal[i0+1]) \
+                    *  ( lon[i0+1] - lon[i0] ) / (lon[i0+2] - lon[i0+1])
+    na_rhoo[i0,j0] = na_rhoo[i0+1,j0] \
+                    + (na_rhoo[i0+2] - na_rhoo[i0+1]) \
+                    *  ( lon[i0+1] - lon[i0] ) / (lon[i0+2] - lon[i0+1])
+  #  # check h extrapolation
+  #  p lon[i0..i0+3]
+  #  for j in j0-1..j0+2
+  #    puts "#{dep[j]}m: #{na_ptemp[i0,j].round(2)} #{na_ptemp[i0+1, j].round(2)}, #{na_ptemp[i0+2, j].round(2)}, #{na_ptemp[i0+3, j].round(2)}, " 
+  #  end
+
+
   # vertical extrapolation
   for i in 0..xn-1
   for j in 1..yn-2
   # ToDo: if miss at j = 0?
-    if na_cptemp[i,j+1] < miss_val+1
-      na_cptemp[i,j+1] = na_cptemp[i,j] \
-          + ( na_cptemp[i,j] - na_cptemp[i,j-1]) \
+    if na_ptemp[i,j+1] < miss_val+1
+      na_ptemp[i,j+1] = na_ptemp[i,j] \
+          + ( na_ptemp[i,j] - na_ptemp[i,j-1]) \
           * ( dep[j+1] - dep[j] ) / ( dep[j] - dep[j-1] )
-      na_csal[i,j+1] = na_csal[i,j] \
-          + ( na_csal[i,j] - na_csal[i,j-1]) \
+      na_sal[i,j+1] = na_sal[i,j] \
+          + ( na_sal[i,j] - na_sal[i,j-1]) \
           * ( dep[j+1] - dep[j] ) / ( dep[j] - dep[j-1] )
-      na_crhoo[i,j+1] = na_crhoo[i,j] \
-          + ( na_crhoo[i,j] - na_crhoo[i,j-1]) \
+      na_rhoo[i,j+1] = na_rhoo[i,j] \
+          + ( na_rhoo[i,j] - na_rhoo[i,j-1]) \
           * ( dep[j+1] - dep[j] ) / ( dep[j] - dep[j-1] )
     end
   end
     # check
     #for j in 0..yn-1
-    #  puts "  #{dep[j]}m: #{na_cptemp[i,j].round(2)}"
+    #  puts "  #{dep[j]}m: #{na_ptemp[i,j].round(2)}"
     #end
   end
 # End: 2016-06-16: test extrapolation
@@ -68,63 +98,64 @@ in_fn = "clim.nc"
   dz = 5.0
   lyn = ( dep.abs.max / dz ).to_i + 1
   ldep = dz * NArray.sfloat( lyn ).indgen * -1.0 + dep.max
-  #na_cptemp = gp_cptemp.val
-  #na_csal   = gp_csal.val
-  #na_crhoo  = gp_crhoo.val
-  cptemp_dlin = NArray.sfloat( xn, lyn ).fill( miss_val )
-  csal_dlin   = NArray.sfloat( xn, lyn ).fill( miss_val )
-  crhoo_dlin  = NArray.sfloat( xn, lyn ).fill( miss_val )
+  #na_ptemp = gp_ptemp.val
+  #na_sal   = gp_sal.val
+  #na_rhoo  = gp_rhoo.val
+  ptemp_dlin = NArray.sfloat( xn, lyn ).fill( miss_val )
+  sal_dlin   = NArray.sfloat( xn, lyn ).fill( miss_val )
+  rhoo_dlin  = NArray.sfloat( xn, lyn ).fill( miss_val )
   for i in 0..xn-1
   #for i in 2..2
     j_end = 0
   for j in 0..yn-2
-    break if na_cptemp[i,j+1] < -100 # check missing
+    break if na_ptemp[i,j+1] < -100 # check missing
     j_bgn = j_end
     for j2 in j_end..lyn-2 
       j_end = j2
       break if (ldep[j2] >= dep[j+1])  and ( ldep[j2+1] < dep[j+1])
     end
-    cptemp_dlin[i,j_bgn] = na_cptemp[i,j]
-    csal_dlin[i,j_bgn] = na_csal[i,j]
-    crhoo_dlin[i,j_bgn] = na_crhoo[i,j]
-    #puts "#{dep[j]}m, #{cptemp_dlin[i,j_bgn]}"
+    ptemp_dlin[i,j_bgn] = na_ptemp[i,j]
+    sal_dlin[i,j_bgn] = na_sal[i,j]
+    rhoo_dlin[i,j_bgn] = na_rhoo[i,j]
+    #puts "#{dep[j]}m, #{ptemp_dlin[i,j_bgn]}"
     for j2 in j_bgn+1..j_end
-      cptemp_dlin[i,j2] = \
-        (  ( ldep[j2] -  dep[j+1] ) * na_cptemp[i,j] \
-         + ( dep[j]   - ldep[j2]  ) * na_cptemp[i,j+1] \
+      ptemp_dlin[i,j2] = \
+        (  ( ldep[j2] -  dep[j+1] ) * na_ptemp[i,j] \
+         + ( dep[j]   - ldep[j2]  ) * na_ptemp[i,j+1] \
         ) / (dep[j] - dep[j+1])
-      csal_dlin[i,j2] = \
-        (  ( ldep[j2] -  dep[j+1] ) * na_csal[i,j] \
-         + ( dep[j]   - ldep[j2]  ) * na_csal[i,j+1] \
+      sal_dlin[i,j2] = \
+        (  ( ldep[j2] -  dep[j+1] ) * na_sal[i,j] \
+         + ( dep[j]   - ldep[j2]  ) * na_sal[i,j+1] \
         ) / (dep[j] - dep[j+1])
-      crhoo_dlin[i,j2] = \
-        (  ( ldep[j2] -  dep[j+1] ) * na_crhoo[i,j] \
-         + ( dep[j]   - ldep[j2]  ) * na_crhoo[i,j+1] \
+      rhoo_dlin[i,j2] = \
+        (  ( ldep[j2] -  dep[j+1] ) * na_rhoo[i,j] \
+         + ( dep[j]   - ldep[j2]  ) * na_rhoo[i,j+1] \
         ) / (dep[j] - dep[j+1])
-    #  puts "  #{ldep[j2]}m: #{cptemp_dlin[i,j2]}"
+    #  puts "  #{ldep[j2]}m: #{ptemp_dlin[i,j2]}"
     end
   end
     if j_end == lyn-2
-      cptemp_dlin[i,lyn-1] = na_cptemp[i,yn-1]
-      csal_dlin[i,lyn-1] = na_csal[i,yn-1]
-      crhoo_dlin[i,lyn-1] = na_crhoo[i,yn-1]
+      ptemp_dlin[i,lyn-1] = na_ptemp[i,yn-1]
+      sal_dlin[i,lyn-1] = na_sal[i,yn-1]
+      rhoo_dlin[i,lyn-1] = na_rhoo[i,yn-1]
       j_end = lyn - 1
     end
-  #  puts "#{lon[i]}E: org: #{na_cptemp[i,0..-1].min}, dint: #{cptemp_dlin[i,j_end]}" 
+  #  puts "#{lon[i]}E: org: #{na_ptemp[i,0..-1].min}, dint: #{ptemp_dlin[i,j_end]}" 
   end
 
 # lon interpolate
   #dx = 0.01
   #dx = 0.001
-  dx = 0.0025 # 250m
+  dx = 0.0025 # ~250m
+    # 2016-06-18: 39.25N > 1 deg E ~ 86.4 km
   lxn = ( ( lon[-1] - lon[0] ) / dx ).to_i + 1
   llon = dx * NArray.sfloat( lxn ).indgen + lon[0]
-  cptemp_lin = NArray.sfloat( lxn, lyn ).fill( miss_val )
-  csal_lin = NArray.sfloat( lxn, lyn ).fill( miss_val )
-  crhoo_lin = NArray.sfloat( lxn, lyn ).fill( miss_val )
+  ptemp_lin = NArray.sfloat( lxn, lyn ).fill( miss_val )
+  sal_lin = NArray.sfloat( lxn, lyn ).fill( miss_val )
+  rhoo_lin = NArray.sfloat( lxn, lyn ).fill( miss_val )
   for j in 0..lyn-1
     for i0 in 0..xn-1
-      break if cptemp_dlin[i0,j] > -100
+      break if ptemp_dlin[i0,j] > -100
     end
     for i2 in 0..lxn-2
       i_end = i2
@@ -138,38 +169,38 @@ in_fn = "clim.nc"
     end
     #puts "org: #{lon[i]}, #{lon[i+1]}, int: #{llon[i_bgn]}, #{llon[i_end]}"
     for i2 in i_bgn..i_end
-      cptemp_lin[i2,j] = \
-        (  ( lon[i+1] - llon[i2]) * cptemp_dlin[i  ,j] \
-         + ( llon[i2] - lon[i]  ) * cptemp_dlin[i+1,j] \
+      ptemp_lin[i2,j] = \
+        (  ( lon[i+1] - llon[i2]) * ptemp_dlin[i  ,j] \
+         + ( llon[i2] - lon[i]  ) * ptemp_dlin[i+1,j] \
         ) / (lon[i+1] - lon[i])
-      csal_lin[i2,j] = \
-        (  ( lon[i+1] - llon[i2]) * csal_dlin[i  ,j] \
-         + ( llon[i2] - lon[i]  ) * csal_dlin[i+1,j] \
+      sal_lin[i2,j] = \
+        (  ( lon[i+1] - llon[i2]) * sal_dlin[i  ,j] \
+         + ( llon[i2] - lon[i]  ) * sal_dlin[i+1,j] \
         ) / (lon[i+1] - lon[i])
-      crhoo_lin[i2,j] = \
-        (  ( lon[i+1] - llon[i2]) * crhoo_dlin[i  ,j] \
-         + ( llon[i2] - lon[i]  ) * crhoo_dlin[i+1,j] \
+      rhoo_lin[i2,j] = \
+        (  ( lon[i+1] - llon[i2]) * rhoo_dlin[i  ,j] \
+         + ( llon[i2] - lon[i]  ) * rhoo_dlin[i+1,j] \
         ) / (lon[i+1] - lon[i])
-    #  puts "  #{llon[i2]}E: #{cptemp_lin[i2,j]}"
-    #  p cptemp_lin[i2,j]
+    #  puts "  #{llon[i2]}E: #{ptemp_lin[i2,j]}"
+    #  p ptemp_lin[i2,j]
     end
   end
-    cptemp_lin[-1,j] = cptemp_dlin[-1,j]
-    csal_lin[-1,j] = csal_dlin[-1,j]
-    crhoo_lin[-1,j] = crhoo_dlin[-1,j]
+    ptemp_lin[-1,j] = ptemp_dlin[-1,j]
+    sal_lin[-1,j] = sal_dlin[-1,j]
+    rhoo_lin[-1,j] = rhoo_dlin[-1,j]
   end
 
 
 # output
-  #axp = GPhys::IO.open( in_fn, "cptemp").get_axparts_k247
+  #axp = GPhys::IO.open( in_fn, "ptemp").get_axparts_k247
   #axp["dep"]["val"] = ldep
   #axp["lon"]["val"] = llon
   axp = set_axp_linterpolate( llon, ldep )
   grid  = GPhys.restore_grid_k247( axp )
-  rslt_fname =  "clim_lin.nc"
+  rslt_fname =  "#{cname}_lin.nc"
   puts "output #{rslt_fname}"
   fu = NetCDF.create( rslt_fname )
-  ["cptemp", "csal", "crhoo"].each do | vname |
+  ["ptemp", "sal", "rhoo"].each do | vname |
     vap = VArray_proto_iwate.new( vname )
       vap.chg_name( "#{vname}_lin" )
       vap.chg_val( eval("#{vname}_lin") )
@@ -178,9 +209,9 @@ in_fn = "clim.nc"
     gp_new = GPhys.new( grid, vap )
     GPhys::NetCDF_IO.write( fu, gp_new )
   end
-    GPhys::NetCDF_IO.write( fu, gp_cptemp )
-    GPhys::NetCDF_IO.write( fu, gp_csal   )
-    GPhys::NetCDF_IO.write( fu, gp_crhoo )
+    GPhys::NetCDF_IO.write( fu, gp_ptemp )
+    GPhys::NetCDF_IO.write( fu, gp_sal   )
+    GPhys::NetCDF_IO.write( fu, gp_rhoo )
   fu.close
 =begin
 =end
